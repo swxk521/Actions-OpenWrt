06496156283671ba8ca3918380b3ddebecffb387
守望 <61259428+swxk521@users.noreply.github.com>
2024-03-03 23:34:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -22,7 +22,7 @@ echo 'src-git diy https://github.com/swxk521/diy' >>feeds.conf.default
 echo 'src-git passwall2 https://github.com/xiaorouji/openwrt-passwall2' >>feeds.conf.default
 echo 'src-git passwallpackages https://github.com/xiaorouji/openwrt-passwall-packages' >>feeds.conf.default
 
-cd..
+cd ..
 git clone https://github.com/kiddin9/openwrt-packages packages
 cp -rf packages/luci-app-kcptun openwrt/package/lean/luci-app-kcptun
 cp -rf packages/luci-app-udp2raw openwrt/package/lean/luci-app-udp2raw
