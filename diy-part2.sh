9e25aef3b2968dc4cf9a3cdb3b600e1f8c720285
守望 <61259428+swxk521@users.noreply.github.com>
2024-03-13 16:40:14 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -27,6 +27,8 @@ rm -rf feeds/luci/applications/luci-app-wrtbwmon
 cp -rf feeds/diy/luci-app-wrtbwmon feeds/luci/applications/luci-app-wrtbwmon
 rm -rf feeds/packages/net/udpspeeder
 cp -rf feeds/diy/udpspeeder feeds/packages/net/udpspeeder
+rm -rf feeds/packages/net/kcptun
+cp -rf feeds/diy/kcptun feeds/packages/net/kcptun
 
 sed -n 's/^PKG_VERSION:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" feeds/diy/changen/passwallMakefile
 cp -f feeds/diy/changen/passwallMakefile feeds/passwall2/luci-app-passwall2/Makefile
