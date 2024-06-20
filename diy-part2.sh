68485acb590fb3fc4132b47913dc509601316d2c
守望 <61259428+swxk521@users.noreply.github.com>
2024-06-20 23:12:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,7 +10,7 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-sed -i 's/default "0"/default "5"/g' config/Config-images.in
+sed -i 's/default "0"/default "1"/g' config/Config-images.in
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/default 400/default 1000/g' config/Config-images.in
 
