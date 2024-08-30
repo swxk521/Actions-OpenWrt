2c9c3b16d1afecb5b5c78b05d7889efd5e3a35c9
守望 <61259428+swxk521@users.noreply.github.com>
2024-08-31 07:12:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,6 +19,8 @@ sed -i 's/autosamba //g' target/linux/x86/Makefile
 sed -i 's/0.53.2/0.60.0/g' feeds/packages/net/frp/Makefile
 sed -i 's/ff2a4f04e7732bc77730304e48f97fdd062be2b142ae34c518ab9b9d7a3b32ec/8feaf56fc3f583a51a59afcab1676f4ccd39c1d16ece08d849f8dc5c1e5bff55/g' feeds/packages/net/frp/Makefile
 
+sed -i '/init.d/d' feeds/packages/net/dante/Makefile
+
 cp -f feeds/diy/changen/targetmk include/target.mk
 rm -rf feeds/packages/net/udpspeeder
 cp -rf feeds/diy/udpspeeder feeds/packages/net/udpspeeder
