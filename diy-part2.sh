7547adeeb55c6d03d2b90c5418699bb19f4a26ef
守望 <61259428+swxk521@users.noreply.github.com>
2024-08-31 13:50:43 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -33,5 +33,4 @@ cp -f feeds/diy/changen/v2ray-geodata feeds/packages/net/v2ray-geodata/Makefile
 cp -f feeds/diy/changen/socat feeds/packages/net/socat/Makefile
 cp -f feeds/diy/changen/luci_socat feeds/luci/applications/luci-app-socat/root/etc/init.d/luci_socat
 cp -f feeds/diy/changen/index.htm package/lean/autocore/files/x86/index.htm
-cp -f feeds/diy/changen/bandwidthd feeds/packages/utils/bandwidthd/files/bandwidthd.config
 cp -f feeds/diy/changen/nginx.config feeds/packages/net/nginx-util/files/nginx.config
