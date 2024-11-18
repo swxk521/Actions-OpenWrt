bb2a5a2e3e1d4378c4b63f2ad34eeb9a9b320e27
守望 <61259428+swxk521@users.noreply.github.com>
2024-11-18 19:36:50 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -15,7 +15,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 sed -i 's/default 448/default 2000/g' config/Config-images.in
 sed -i 's/automount //g' target/linux/x86/Makefile
 sed -i 's/autosamba //g' target/linux/x86/Makefile
-sed -i 's/6\.6/5\.15/g' target/linux/x86/Makefile
+# sed -i 's/6\.6/5\.15/g' target/linux/x86/Makefile
 
 sed -n 's/^PKG_VERSION:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" feeds/diy/changen/passwallMakefile
 cp -f feeds/diy/changen/passwallMakefile feeds/passwall2/luci-app-passwall2/Makefile
