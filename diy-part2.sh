3763517ec285a6c8896ea3c423ddb90a6eec4030
守望 <61259428+swxk521@users.noreply.github.com>
2024-11-21 18:50:31 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,8 +20,8 @@ sed -i 's/6\.6/6\.1/g' target/linux/x86/Makefile
 sed -n 's/^PKG_VERSION:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" feeds/diy/changen/passwallMakefile
 cp -f feeds/diy/changen/passwallMakefile feeds/passwall2/luci-app-passwall2/Makefile
 
-sed -i 's/20230207/20241031/g' feeds/packages/net/kcptun/Makefile
-sed -i 's/09054b52d5799a8e47edb36f2db335d929d5bbb63a26f7ba2fe03c64ec39d550/f3430de60f219bdd8d4d57468e827559791ae63ce0b84f50840b24b45647a8f7/g' feeds/packages/net/kcptun/Makefile
+sed -i 's/20230207/20241119/g' feeds/packages/net/kcptun/Makefile
+sed -i 's/09054b52d5799a8e47edb36f2db335d929d5bbb63a26f7ba2fe03c64ec39d550/a591b539e6a0d2a3b652fa5825fc81b4c3b087412d8692403b0b831fd11014b2/g' feeds/packages/net/kcptun/Makefile
 
 sed -i 's/0.51.3/0.61.0/g' feeds/packages/net/frp/Makefile
 sed -i 's/83032399773901348c660d41c967530e794ab58172ccd070db89d5e50d915fef/c06a11982ef548372038ec99a6b01cf4f7817a9b88ee5064e41e5132d0ccb7e1/g' feeds/packages/net/frp/Makefile
