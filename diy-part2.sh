6ccd6f45e92ac1e03aab56e0e3e295a7b6e9a38d
root <root@debian.debian>
2026-05-21 14:21:20 +0800

    pr

---
+++ diy-part2.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+# Modify default IP
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
+
+
+sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/luci/bin/config_generate
+sed -i 's/default 448/default 2000/g' config/Config-images.in
+
+sed -n 's/^PKG_VERSION:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" feeds/diy/changen/passwallMakefile
+sed -n 's/^PKG_RELEASE:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYRELEASE/{}/g" feeds/diy/changen/passwallMakefile
+cp -f feeds/diy/changen/passwallMakefile feeds/passwall2/luci-app-passwall2/Makefile
+
+cp -f feeds/diy/changen/99-sysctl.conf package/base-files/files/etc/sysctl.d/99-sysctl.conf
+sed -i '/sysctl/d' feeds/packages/net/nlbwmon/Makefile
+cp -f feeds/diy/changen/dnsmasq.init package/network/services/dnsmasq/files/dnsmasq.init
+cp -f feeds/diy/changen/targetmk include/target.mk
+cp -f feeds/diy/changen/index.htm package/lean/autocore/files/x86/index.htm
+
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
