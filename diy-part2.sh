96f59e37c613a685f9fc9953dddf5bc94e93bdb9
守望 <61259428+swxk521@users.noreply.github.com>
2026-05-22 16:55:41 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -23,12 +23,12 @@
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
 sed -i 's/default 160/default 2000/g' config/Config-images.in
 
-sed -n 's/^PKG_VERSION:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" feeds/diy/changen/passwallMakefile
-sed -n 's/^PKG_RELEASE:=//p' feeds/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYRELEASE/{}/g" feeds/diy/changen/passwallMakefile
-cp -f feeds/diy/changen/passwallMakefile feeds/passwall2/luci-app-passwall2/Makefile
+sed -n 's/^PKG_VERSION:=//p' package/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYVERSION/{}/g" package/diy/changen/passwallMakefile
+sed -n 's/^PKG_RELEASE:=//p' package/passwall2/luci-app-passwall2/Makefile | xargs -I {} sed -i "s/DIYRELEASE/{}/g" package/diy/changen/passwallMakefile
+cp -f package/diy/changen/passwallMakefile package/passwall2/luci-app-passwall2/Makefile
 
-cp -f feeds/diy/changen/99-sysctl.conf package/base-files/files/etc/sysctl.d/99-sysctl.conf
-cp -f feeds/diy/changen/X86Makefile target/linux/x86/Makefile
+cp -f package/diy/changen/99-sysctl.conf package/base-files/files/etc/sysctl.d/99-sysctl.conf
+cp -f package/diy/changen/X86Makefile target/linux/x86/Makefile
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
