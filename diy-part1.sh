ec1bb72b1f15302603b9dd30b3015ebbe759d290
守望 <61259428+swxk521@users.noreply.github.com>
2026-05-24 10:29:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-git clone https://github.com/swxk521/diy1 package/diy
+git clone https://github.com/swxk521/diy package/diy
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall2 package/passwall2
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwallpackages
 git clone https://github.com/timsaya/luci-app-bandix package/luci-app-bandix
